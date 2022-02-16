37cc44905a7984ec048daf9b43614d194b900cec
LebJe <51171427+LebJe@users.noreply.github.com>
2022-02-16 08:42:14 -0500

    Commit

--- Sources/ArcGISKit/HandleResponse.swift
+++ Sources/ArcGISKit/HandleResponse.swift
@@ -15,30 +15,32 @@ func handle<T: Codable>(response: HTTPClient.Response, decodeType: T.Type) throw
 			return try XJSONDecoder().decode(decodeType, from: Array(buffer: response.body!))
 		} catch {
 			throw error
-			// do {
-			// 	let re = try JSONDecoder().decode(ResponseError.self, from: Data(buffer: response.body!))
+			do {
+				let re = try JSONDecoder().decode(ResponseError.self, from: Data(buffer: response.body!))
+	
+				throw AGKRequestError.unknown(message: re.error?.message, details: re.error?.details)
 
-			// 	switch re.error?.message.lowercased() ?? "" {
-			// 		case "Invalid username or password.".lowercased():
-			// 			throw AGKAuthError.invalidUsernameOrPassword
-			// 		case "Token Required".lowercased():
-			// 			throw AGKRequestError.tokenRequired
-			// 		case "Unable to generate token.".lowercased():
-			// 			if let details = re.error?.details {
-			// 				if details.contains("Invalid username or password.") {
-			// 					throw AGKAuthError.invalidUsernameOrPassword
-			// 				} else {
-			// 					throw AGKRequestError.unknown(message: re.error?.message, details: re.error?.details)
-			// 				}
-			// 			} else {
-			// 				throw AGKRequestError.unknown(message: re.error?.message, details: re.error?.details)
-			// 			}
-			// 		default:
-			// 			throw AGKRequestError.unknown(message: re.error?.message, details: re.error?.details)
-			// 	}
-			// } catch {
-			// 	throw error
-			// }
+				// switch re.error?.message.lowercased() ?? "" {
+				// 	case "Invalid username or password.".lowercased():
+				// 		throw AGKAuthError.invalidUsernameOrPassword
+				// 	case "Token Required".lowercased():
+				// 		throw AGKRequestError.tokenRequired
+				// 	case "Unable to generate token.".lowercased():
+				// 		if let details = re.error?.details {
+				// 			if details.contains("Invalid username or password.") {
+				// 				throw AGKAuthError.invalidUsernameOrPassword
+				// 			} else {
+				// 				throw AGKRequestError.unknown(message: re.error?.message, details: re.error?.details)
+				// 			}
+				// 		} else {
+				// 			throw AGKRequestError.unknown(message: re.error?.message, details: re.error?.details)
+				// 		}
+				// 	default:
+				// 		throw AGKRequestError.unknown(message: re.error?.message, details: re.error?.details)
+				// }
+			} catch {
+				throw error
+			}
 		}
 	} else {
 		throw AGKRequestError.unknown(message: "No response", details: nil)
